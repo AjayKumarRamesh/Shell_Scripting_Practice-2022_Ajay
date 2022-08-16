0f3426f12e3e3dd2e3a39b3ccecffd3c5980c623
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 16:00:15 +0530

    continue_loops.sh

---
+++ Day-11/continue_loops.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# this is for Continue command or loop
+# continue command is used in the script to skip current iteration of loop and continue to next iteration of the loop.
+
+for each in 10 20 30 40 50
+do
+	if [ $each -eq 30 ]
+	then
+	 continue
+	fi
+	echo "$each"
+done 
