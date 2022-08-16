e739fa6a3944e3f52a2a8d702ae234d1ce736fa5
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 15:53:27 +0530

    system_define-variables.sh

---
+++ Day-3/system_define-variables.sh
@@ -0,0 +1,13 @@
+command to check system defined variables 
+env
+print env 
+set | grep USER
+env | grep USER 
+cho "$USER"
+echo "$SHELL"
+NOTE:-->> always our system define variable we should enter in capitals and they are in capitals to see that one
+
+
+command to check in shell previosusly used executed command status or previosus command which is got success
+echo $?
+
