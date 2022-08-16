25f25775edb3b9ef6e3e1237cf2eeea992292ccc
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 15:57:33 +0530

    start_stop_httpd_if_statement.sh

---
+++ Day-7/start_stop_httpd_if_statement.sh
@@ -0,0 +1,24 @@
+# this below statement condition is used to start or stop httpd
+
+read -p "enter start or stop to perform action on httpd service:" action
+
+if [ "$(action}" == "start"]
+then
+      
+   echo " the httpd service is starting"	
+   sudo systemctl start httpd
+   echo "starting httpd"
+fi
+
+
+
+if  [ "${action}" == "stop"]
+then
+ 
+  echo "the httpd service is stopping"
+  sudo systemctl stop httpd
+  echo "stopping the httpd service "
+
+then
+
+fi	
