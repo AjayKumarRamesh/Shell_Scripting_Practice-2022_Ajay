c3765d571e4143397a067b61bf5104ea4b54e632
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 15:59:32 +0530

    while_loop.sh

---
+++ Day-10/while_loop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+<< com1
+while date &> /dev/null
+do
+echo "date command is executing with while loop"
+done
+com1
+
+<< com2
+while [[ 4 -gt 1 ]]
+do
+echo "executing while loop"
+echo "================"
+done
+com2
+
+
+start=1
+while [[ $start -le 10 ]]
+do
+echo "$start"	
+((start++))
+done
+
+
+
