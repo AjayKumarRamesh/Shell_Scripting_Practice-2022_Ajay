2d02a7a317ecc8544a92ed170ff8bce23eabd024
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 15:58:17 +0530

    seq_of_num.sh

---
+++ Day-9/seq_of_num.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+<< comm
+This topic about:-
+1.How to Generate Sequence of Numbers?
+2.Why we need this Sequence of Numbers?
+
+Generating sequence of Numbers in two ways:-
+1.Using seq command
+2.Using brace expansion
+
+Steps of sequence :-
+1.seq start end
+2. seq start step end
+3. echo {start...end}
+4. echo {start...end..step}
+
+2.Why we need this sequence of numbers?
+1. Very useful in loops
+comm
+
