b92bdc86465bc31c8ff3878df18aa67dcc54b7e4
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 15:54:18 +0530

    echo_command_information.sh

---
+++ Day-4/echo_command_information.sh
@@ -0,0 +1,11 @@
+echo command meanings or information:-
+1. this is a built command that is mostly used in shell script to display 
+a. any string/text/message
+b. any variable
+c. any command output
+
+Basic syntax of echo command is:-
+a. echo string/text/message
+b. echo $variable 
+c. echo $(command)
+
