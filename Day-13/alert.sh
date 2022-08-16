3318093bbf3cc28ed8de72d84c74ddc7f0be5b76
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 16:01:05 +0530

    alert.sh

---
+++ Day-13/alert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#This topic is about a shell script to send E-mail alert a memory is low or full
+
+TO="ajaykumar.matters@gmail.com"
+ram_free=$(free -mt | grep Total | awk '{print $4}')
+
+if [ $ram_free -le 4000 ]
+then 
+	echo "your ram size is less then 4677 mb"
+	echo "subject: warning, RAM FREE SIZE is low"|sendmail $TO
+fi
+
+
+
