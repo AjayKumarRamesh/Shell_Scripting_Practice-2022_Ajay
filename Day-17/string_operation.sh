e8cbe38e9ec29d7df1efac5b6ede94748cb5ade3
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 16:02:43 +0530

    string_operation.sh

---
+++ Day-17/string_operation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This topic is about string and operations 
+<< com
+command to delete the substring matching
+path_tom="/opt/apache-tomcat-9.6.0.tar.gz"
+echo "${path_tom%tar.gz}" -->> this using % command we ca =n delete the end of the file location or make it short 
+echo "${oath_tom#/opt}" -->> using this # symbol we can delete in the beginning of path 
+com
