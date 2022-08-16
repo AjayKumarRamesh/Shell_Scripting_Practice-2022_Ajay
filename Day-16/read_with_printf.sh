5ab1aa85dc27f708e20d26a133a750506e42e4dc
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 16:02:20 +0530

    read_with_printf.sh

---
+++ Day-16/read_with_printf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is about printf command .
+# printf command is printed with pre-formatted output
+# it is similiar to the printf() of c programming language 
+
+#prinft syntax:
+
+#print <formate> <arguments>
+#formate is <optional> we can omit it
+<< com
+echo "enter your value"
+read x
+eccho "the value of x is: $x"
+com
+
+
+#printf eg:-
+
+printf "enter the valure of x: \n"
+read x
+echo "the value of x is: $x"
