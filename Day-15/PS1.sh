5a2eeba1b9825847cdebb38b916c578cee87b0d9
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 16:01:55 +0530

    PS1.sh

---
+++ Day-15/PS1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This topic is about PS-1 prompt environment variable
+# PS-1 is called primary prompt string and it is the default interaction prompt 
+# this about using export for our ~/.bashrc file 
+
+
