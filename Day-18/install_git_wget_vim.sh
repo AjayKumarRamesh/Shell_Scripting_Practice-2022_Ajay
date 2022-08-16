143bdb5a056f1e1be144d24d2794a0dfe1701020
Ajay Kumar R <akumarr2@in.ibm.com>
2022-08-16 16:03:04 +0530

    install_git_wget_vim.sh

---
+++ Day-18/install_git_wget_vim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# user is with root or sudo priveliages 
+<< com
+if [ $(whoami) != "root" ]
+then
+	echo "please run this shell scripting with root user or sudo priviliages"
+	exit 1
+fi
+com
+
+
+
+
+if [ $(id-u) -ne 0 ]
+then 
+	echo "plese run this shell script with root user or sudo privileages"
+	exit 1
+fi
+
+apt install wget -y
+apt install vim -y
+apt install git -y 
