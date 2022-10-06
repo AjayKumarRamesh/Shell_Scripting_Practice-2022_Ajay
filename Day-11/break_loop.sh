71acb9b4c0ec7d926a95aa9bbec34619dfd1dff3
AjayKumarRamesh <114361627+AjayKumarRamesh@users.noreply.github.com>
2022-10-06 23:20:34 +0530

    Update break_loop.sh

--- Day-11/break_loop.sh
+++ Day-11/break_loop.sh
@@ -15,5 +15,18 @@ do
 
    i=$((i+1))
 done
+###########################################33
 
+#Start of for loop
+for a in 1 2 3 4 5 6 7 8 9 10
+do
+    # if a is equal to 5 break the loop
+    if [ $a == 5 ]
+    then
+        break
+    fi
+    # Print the value
+    echo "Iteration no $a"
+done
+#############################################################3
 
